4c8dcf2a3155d8b7a1b2aee5db929a213d17ca3d
Jdunlap0004 <jack.dunlap04@gmail.com>
2026-02-05 17:26:23 -0800

    WASD movement

--- public/game.js
+++ public/game.js
@@ -5,25 +5,53 @@ const config = {
     parent: 'game-container',
     physics: {
         default: 'arcade',
-        arcade: { gravity: { y: 5000 } }
+        arcade: { gravity: { y: 300 } }
     },
-    scene: { preload, create }
+    scene: { preload, create, update }
 };
 
 const game = new Phaser.Game(config);
+let player;
+let cursors;
 
 function preload() {
-    // This is where you'll later load your .txt levels!
+    //load txt files
 }
 
 function create() {
-    this.add.text(400, 100, 'TEXTPLORER: HELLO WORLD', { 
+    this.add.text(400, 100, 'TEXTPLORER: WASD to move', { 
         fontSize: '32px', 
         fill: '#fff' 
     }).setOrigin(0.5);
 
-    // A simple physics box to test the engine
+    player = this.add.rectangle(400, 300, 50, 50, 0x00ff00);
+    this.physics.add.existing(player);
+    player.body.setCollideWorldBounds(true);
+
+    cursors = this.input.keyboard.addKeys({
+        up: Phaser.Input.Keyboard.KeyCodes.W,
+        down: Phaser.Input.Keyboard.KeyCodes.S,
+        left: Phaser.Input.Keyboard.KeyCodes.A,
+        right: Phaser.Input.Keyboard.KeyCodes.D
+    });
+
     const logo = this.add.rectangle(400, 300, 50, 50, 0x00ff00);
     this.physics.add.existing(logo);
     logo.body.setBounce(0.8).setCollideWorldBounds(true);
+}
+
+function update() {
+    const speed = 200;
+
+    if (cursors.left.isDown) {
+        player.body.setVelocityX(-speed);
+    } else if (cursors.right.isDown) {
+        player.body.setVelocityX(speed);
+    } else {
+        player.body.setVelocityX(0);
+    }
+
+    if (cursors.up.isDown && player.body.blocked.down) {
+        player.body.setVelocityY(-350);
+    }
 }
